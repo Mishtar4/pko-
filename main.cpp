f8ac0bfd28d6d5dcb3fa041b81b7b8ad8062e22d
Mishtar4 <chernatamisha@gmail.com>
2025-11-17 15:57:05 +0100

    ДВщDodano zapis do pliku. Nowe menu zarzadzaniem konta

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main() {
     vector <Klient> zakonczone; // log zakończonych rozmów
 
     bool maAktywna = false; // czy trwa rozmowa?
-    Klient aktywna("", "");
+    Klient aktywna;
     // aktualnie obsługiwany klient (jeśli jest)
 
     int wybor;
@@ -50,17 +50,25 @@ int main() {
         "3. Wstrzymaj aktywna rozmowe (na stos)\n" <<
         "4. Wznow rozmowe (ze stosu)\n" <<
         "5. Zakoncz aktywna rozmowe\n" <<
-        "6. Podglad stanu\n" <<
-        "7. Wyjdz\n" <<
-        "Wybor: ";
+        "6. Podglad stanu\n";
+        if (maAktywna) {
+            cout << "7. Operacja na koncie aktywnego klienta" <<  endl;
+        }
+        cout << "0. Wyjdz\n" << "Wybor: ";
         cin >> wybor;
         cin.ignore();
+
+        if (!maAktywna && wybor == 7) {
+            cout << "Nie ma takiej opcji." << endl;
+            continue;
+        }
         switch (wybor) {
             case 1: {
                 // Nowe zgłoszenie trafia do kolejki
                 cout << "Wpisz imie: ";
                 string imie;
                 getline(cin, imie);
+
                 //Tworzenie nowego klienta
                 Klient kl;
 
@@ -80,7 +88,8 @@ int main() {
                 cout << "Opisz problem: ";
                 string problem;
                 getline(cin, problem);
-                kl = (imie, problem);
+                kl.imie = imie;
+                kl.problem = problem;
                 oczekujacy.push(kl);
                 break;
             }
@@ -201,6 +210,49 @@ int main() {
                 break;
             }
             case 7: {
+                int op;
+                bool case5 = false;
+                while (true) {
+                    cout << "\n==== Operacji z kontem ====\n" <<
+                    "1. Dodaj przychod\n" <<
+                    "2. Dodaj wydatek\n" <<
+                    "3. Pokaz aktualne saldo\n" <<
+                    "4. Zmien walute\n" <<
+                    "0. Zakoncz dzialanie\n" <<
+                    "Wybor: ";
+                    cin >> op;
+                    switch (op) {
+                        case 1: aktywna.konto.dodajPrzychod(); break;
+                        case 2: aktywna.konto.dodajWydatek(); break;
+                        case 3: aktywna.konto.pokazSaldo(); break;
+                        case 4: aktywna.konto.zmienWalute(); break;
+                        case 0: {
+                            case5 = true;
+                            string plik_nazwa = aktywna.imie + ".txt";
+                            ofstream file(plik_nazwa);
+                            if (file.is_open()) {
+                                file << aktywna.konto.saldo << endl;
+                                file << aktywna.konto.waluta << endl;
+                                file << aktywna.konto.ostatniaOperacja << endl;
+                                file.close();
+                            } else {
+                                cout << "Nie udalo sie nadpisac pliku." << endl;
+                            }
+                            break;
+                        }
+                        default: {
+                            cout << "Nie ma takiej opcji." << endl;
+                            break;
+                        }
+                    }
+                    if (case5) {
+                        cout << "Koniec pracy z kontem." << endl;
+                        break;
+                    }
+                }
+                break;
+            }
+            case 0: {
                 cout << "Koniec pracy." << endl;
                 return 0;
             }
